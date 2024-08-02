c73d80f646903d589cffe7453b3d0233c4829550
Matan Bardugo <matanew1@gmail.com>
2024-08-02 11:36:58 +0300

    added todo

--- app/src/main/java/com/example/tripmaster/MyApp.java
+++ app/src/main/java/com/example/tripmaster/MyApp.java
@@ -6,6 +6,8 @@ import com.example.tripmaster.Utils.FireBaseOperations;
 
 public class MyApp extends Application {
 
+    //TODO: fix vars name in xml
+    //TODO; check bugs deserialize
     @Override
     public void onCreate() {
         super.onCreate();
