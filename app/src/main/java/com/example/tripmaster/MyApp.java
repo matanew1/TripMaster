9b8794181ab68c0b0b2952a813013d3290aa88fa
Matan Bardugo <matanew1@gmail.com>
2024-08-02 11:40:11 +0300

    added todo

--- app/src/main/java/com/example/tripmaster/MyApp.java
+++ app/src/main/java/com/example/tripmaster/MyApp.java
@@ -8,6 +8,8 @@ public class MyApp extends Application {
 
     //TODO: fix vars name in xml
     //TODO; check bugs deserialize
+    //TODO: added timeline view for a trip
+    //TODO: improve design in the UI
     @Override
     public void onCreate() {
         super.onCreate();
